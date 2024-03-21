df05060a5fbe037b682f206f47e6564d4abd3894
scarlett-troncoso <scar.labussieri@gmail.com>
2024-03-21 18:07:40 +0100

    created method function constructor

--- index.php
+++ index.php
@@ -7,28 +7,37 @@ class Movie{
     public $overview;
     public $duration;
     public $cast;
+
+    public function __construct($title, $overview, $duration, $cast)
+    {
+        $this-> title = $title;
+        $this-> overview = $overview;
+        $this-> duration = $duration;
+        $this-> cast = $cast;
+    }
+
 };
 
 //create an instance of a class (an object) with the 'new' keyword
-$harryPotter = new Movie;
+$harryPotter = new Movie('Harry Potter', 'Harry Potter Lorem ipsum dolor ecc', 120, []);
 
 //Write values to each property of the object
 $harryPotter->title = 'Harry Potter';
 $harryPotter->overview = 'Harry Potter Lorem ipsum dolor ecc';
 $harryPotter->duration = 120;
-$harryPotter->cast = 'Daniel Radcliff';
+$harryPotter->cast = [ "Daniel Radcliffe", "Rupert Grint", "Emma Watson"];
 
 var_dump($harryPotter);
 
 
 //create a second instance
-$poorThings = new Movie;
+$poorThings = new Movie('Poor Things', 'Poor Things.... Lorem ipsum dolor ecc', 90, [] );
 
 //Write values to each property of the object
 $poorThings->title = 'Poor Things';
 $poorThings->overview = 'Poor Things.... Lorem ipsum dolor ecc';
 $poorThings->duration = 90;
-$poorThings->cast = 'Emma Stone';
+$poorThings->cast = ["Emma Stone", "Mark Ruffalo", "Willem Dafoe", "Ramy Youssef", "Christopher Abbott", "Jerrod Carmichael"];
 
 var_dump($poorThings);
 

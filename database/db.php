712f98405b3601ddc45cea8e3bd66a39fc6b6c05
scarlett-troncoso <scar.labussieri@gmail.com>
2024-03-21 22:17:31 +0100

    add new Genre class in movie and file genre with require

--- database/db.php
+++ database/db.php
@@ -1,14 +1,26 @@
 <?php 
+require_once __DIR__ . '/../app/Models/Movie.php';
+require_once __DIR__ . '/../app/Models/Genre.php';
 
 //$movies = [$harryPotter, $poorThings]
 
 $movies = [
-    new Movie('Harry Potter', 'Harry Potter Lorem ipsum dolor ecc', 120, [
+    new Movie('Harry Potter', 'Harry Potter Lorem ipsum dolor ecc', 120,
+        [ 
+        new Genre('Molto Drammatico', 'fastastico'), // questa é una instanza dentro un array
+        new Genre('Molto fantasioso', 'molto-ecc'),
+        ], 
+        [
         "Daniel Radcliffe", 
         "Rupert Grint", 
         "Emma Watson"
     ]),
-    new Movie('Poor Things', 'Poor Things.... Lorem ipsum dolor ecc', 90, [
+    new Movie('Poor Things', 'Poor Things.... Lorem ipsum dolor ecc', 90,
+        [ 
+        new Genre('Molto Drammatico', 'fastastico'), // questa é una instanza dentro un array
+        new Genre('Molto fantasy', 'molto-ecc'),
+        ], 
+        [
         "Emma Stone", 
         "Mark Ruffalo", 
         "Willem Dafoe", 

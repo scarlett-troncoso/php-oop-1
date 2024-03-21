65b6bed78271c4d31ed26fdd2ed62bda063f1b01
scarlett-troncoso <scar.labussieri@gmail.com>
2024-03-21 17:22:03 +0100

    create two instances for the class movie

--- index.php
+++ index.php
@@ -1,16 +1,36 @@
 <?php
-// Class declaration
 /**
  * Class Movie
  */
 class Movie{
-
+    public $title;
+    public $overview;
+    public $duration;
+    public $cast;
 };
 
 //create an instance of a class (an object) with the 'new' keyword
-$padrino = new Movie;
+$harryPotter = new Movie;
 
-var_dump($padrino);
+//Write values to each property of the object
+$harryPotter->title = 'Harry Potter';
+$harryPotter->overview = 'Harry Potter Lorem ipsum dolor ecc';
+$harryPotter->duration = 120;
+$harryPotter->cast = 'Daniel Radcliff';
+
+var_dump($harryPotter);
+
+
+//create a second instance
+$poorThings = new Movie;
+
+//Write values to each property of the object
+$poorThings->title = 'Poor Things';
+$poorThings->overview = 'Poor Things.... Lorem ipsum dolor ecc';
+$poorThings->duration = 90;
+$poorThings->cast = 'Emma Stone';
+
+var_dump($poorThings);
 
 
 

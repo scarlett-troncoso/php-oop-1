a563ed1e8887c6582f8ffa51315a26d5f5791062
scarlett-troncoso <scar.labussieri@gmail.com>
2024-03-21 16:48:07 +0100

    initial commit, and create first file

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+// Class declaration
+/**
+ * Class Movie
+ */
+class Movie{
+
+};
+
+//create an instance of a class (an object) with the 'new' keyword
+$padrino = new Movie;
+
+var_dump($padrino);
+
+
+
+
+?>

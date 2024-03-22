9886ac962f56551ce48a4a1015b3cb3fb859bbfa
scarlett-troncoso <scar.labussieri@gmail.com>
2024-03-22 11:57:17 +0100

    add static property and method static

--- app/Models/Movie.php
+++ app/Models/Movie.php
@@ -4,13 +4,16 @@
  * Class Movie
  */
 class Movie{
-    public $title;
+
+    public static string $type = 'Current Movie'; //proprieta static
+
+    /*public $title;
     public $overview;
     public $duration;
     public $genres;
-    public $cast;
+    public $cast;*/ 
 
-    public function __construct($title, $overview, $duration, array $genres, $cast)
+    public function __construct(public string $title, public string $overview, public int $duration, public array $genres, public array $cast) 
     {
         $this-> title = $title;
         $this-> overview = $overview;
@@ -25,6 +28,14 @@ class Movie{
 
     public function getCast(){ //legge un valore
         return $this->cast;
+    }
+
+    public function getNoStaticType(){ //metodo 'normale' nel quale usiamo la proprietá statica
+        return self::$type; //cosi con self:: puo leggere le proprietá di questa istanza 'Address' pero SOLO di quelle static
+    }
+
+    public static function getStaticType(){ //metodo statico che chiamiamo staticamente
+        return self::$type;
     }
 };
 

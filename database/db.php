843ad23f83f160f5adc7a766b1424b8828748aec
scarlett-troncoso <scar.labussieri@gmail.com>
2024-03-27 22:09:02 +0100

    transform isstance new Genre in array

--- database/db.php
+++ database/db.php
@@ -6,20 +6,16 @@ require_once __DIR__ . '/../app/Models/Genre.php';
 
 $movies = [
     new Movie('Harry Potter', 'Harry Potter Lorem ipsum dolor ecc', 120,
-        [ 
-        new Genre('Molto Drammatico', 'fastastico'), // questa é una instanza dentro un array
-        new Genre('Molto fantasioso', 'molto-ecc'),
-        ], 
+        new Genre(['Molto Drammatico', 'fastastico'] ), // questa é un'instanza dentro un array
+        //new Genre('Molto fantasioso', 'molto-ecc'), 
         [
         "Daniel Radcliffe", 
         "Rupert Grint", 
         "Emma Watson"
     ]),
     new Movie('Poor Things', 'Poor Things.... Lorem ipsum dolor ecc', 90,
-        [ 
-        new Genre('Molto Drammatico', 'fastastico'), // questa é una instanza dentro un array
-        new Genre('Molto fantasy', 'molto-ecc'),
-        ], 
+        new Genre(['Molto Drammatico', 'fastastico']), // questa é una instanza dentro un array
+        //new Genre('Molto fantasy', 'molto-ecc'),
         [
         "Emma Stone", 
         "Mark Ruffalo", 
